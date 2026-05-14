804a2e6e057b9519d22d2ff039467e4ce4a4d3a4
thuongcjn <lecongthuong2004@gmail.com>
2026-05-14 12:21:40 +0700

    fix bug send mail

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -3,13 +3,14 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   // Create transporter
   const transporter = nodemailer.createTransport({
-    // host: process.env.SMTP_HOST,
-    // port: process.env.SMTP_PORT,
-    service: 'gmail',
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
+    family: 4,
   });
 
   // Define message
