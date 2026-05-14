064cff5e3c8fc8f0482ade4d3f0d0bf76f867929
thuongcjn <lecongthuong2004@gmail.com>
2026-05-14 13:18:08 +0700

    restore

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -5,12 +5,10 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
-    family: 4,
   });
 
   // Define message
